dfb861d4c46a217777e3fd6e8f2b6f44189b676b
tatuki <b1402823@planet.kanazawa-it.ac.jp>
2015-06-30 08:51:30 +0900

    追加： 現時点での作成したスクリプト

---
+++ WolfDensanProject/Assets/script/Manager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+//ゲームマネージャースクリプト
+public class Manager : MonoBehaviour {
+
+	public float time;//ゲーム開始時からカウントする時間（コンマ秒）
+	public int time_second;//ゲーム開始時からカウントする(秒）
+	public int frame_count;//ゲーム開始時からカウントするフレームカウント
+
+	public Text score_text;//スコアポイントのテキスト
+	public int score;//加算するスコアポイント
+	private int score_retain;//スコア保持
+
+	public int balet_count;//弾を打つ間隔
+
+	public float screen_width = Screen.width;//画面サイズ取得
+	public float screen_height = Screen.height;//画面サイズ取得
+
+
+	// Use this for initialization
+	void Start () {
+
+		//初期スコアの表示
+		score_text.text = "SCORE:"+score_retain;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		screen_width = Screen.width; //画面横サイズ
+		screen_height = Screen.height ;//画面縦サイズ
+
+		//時間のカウント
+		time += Time.deltaTime;
+		time_second = (int)time;
+		//バレット間隔のカウント
+		balet_count ++;
+
+		//フレームの加算
+		frame_count++;
+
+		//スコアが変わったら変更
+		if(score_retain != score){
+			score_retain += 5;
+			score_text.text = "SCORE:"+score_retain;
+		}
+
+	}
+}
---
+++ WolfDensanProject/Assets/script/Shot_Cnnect/balet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//バレット自身のスクリプト
+//マウスの方向へ飛んでいき、一定時間で消滅する
+public class balet : MonoBehaviour {
+
+	public float power = 100f;//力
+	public float des_time = 1f;//消す時間
+
+	// Use this for initialization
+	void Start () {
+
+		Vector3 v3mouse = Input.mousePosition;//マウス座標
+
+		//マウスの光線
+		Ray rmouse;
+		//マウス座標から光線の発射
+		rmouse = Camera.main.ScreenPointToRay(v3mouse);
+
+		//光線の向きをvector3に変換
+		Vector3 diff  = rmouse.direction.normalized;
+
+		//光線の向きに飛ばす
+		this.GetComponent<Rigidbody>().AddForce(diff * power,ForceMode.Impulse);
+		//数秒後に破壊
+		Destroy(gameObject,des_time);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
