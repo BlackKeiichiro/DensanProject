789da7a3c3e5f9efda0b68a05a4f36decc9a9f6d
BLACK <blackk16.k@gmail.com>
2015-09-09 21:45:09 +0900

    追加・変更： ・ステージを新しいモデルを差し替えた。それに合わせたキャラクターの移動、座標を変更。 ・ゲージUI(進捗40％程) 今後、アイテムをどのように配置するのか実装する必要がある。

---
+++ Assets/PartA/Script/Item/ItemInstantiate.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+public class ItemInstantiate : MonoBehaviour {
+	private float[] item_x = new float[]{-12,-6,0,6,12};
+	private GameObject[] type_items;
+	private List<Transform> itemzone_child = new List<Transform>();
+	private Manager manager;
+	//public Item[] instant_items;
+
+	void Awake(){
+		foreach(Transform child in this.transform){
+			itemzone_child.Add(child);
+		}
+		manager = GameObject.Find("Manager").GetComponent<Manager>();
+		type_items = new GameObject[]{
+			 Resources.Load("Prefabs/Wall") as GameObject,
+			 Resources.Load("Prefabs/Weapon") as GameObject,
+		};
+	}
+	void Start () {
+		int random = Random.Range(0,1);
+		GameObject[] localobject = new GameObject[item_x.Length];
+		for(int zoneindex = 0;zoneindex < itemzone_child.Count;zoneindex++){
+			for(int itemindex = 0;itemindex < item_x.Length;itemindex++){
+				localobject[itemindex] = Instantiate(type_items[manager.patternlist[0,zoneindex,itemindex]]) as GameObject;
+				localobject[itemindex].transform.parent = itemzone_child[zoneindex];
+				localobject[itemindex].transform.localPosition = new Vector3(item_x[itemindex],0,0);
+				localobject[itemindex].transform.localRotation = Quaternion.Euler(Vector3.zero);
+			}
+		}
+	}
+}
---
+++ Assets/PartA/Script/Manager/TowerManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerManager : MonoBehaviour {
+	[SerializeField]
+	private GameObject[] tower;
+	private GameObject _player;
+	private float between_margin;
+	//private int nowtower = 0;
+	// Use this for initialization
+	void Start () {
+		tower = new GameObject[3]{GameObject.Find("tou0"),GameObject.Find("tou1"),GameObject.Find("tou2")};
+		_player = GameObject.Find("Players");
+		between_margin = Mathf.Abs(tower[0].transform.position.y - tower[1].transform.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(tower[0].transform.position.y > _player.transform.position.y){//between_margin*2 < Mathf.Abs(tower[0].transform.position.y - _player.transform.position.y)){
+			//tower[0].transform.position -= Vector3.up*between_margin*3;
+			GameObject localObject = Instantiate(Resources.Load("Prefabs/maintou"),tower[2].transform.position - Vector3.up*between_margin,Quaternion.Euler(0,160,0)) as GameObject;
+			Destroy(tower[0]);
+			tower = new GameObject[3]{tower[1],tower[2],localObject};
+			//nowtower = (nowtower>1)?0:++nowtower;
+		}
+	}
+}
